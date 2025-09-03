98cabd9f5f7e6f8b53ed894fe7bd2a643f832488
ael-kass <aelkassimi321@gmail.com>
2025-09-03 10:05:42 +0100

    add the future moving around to the player

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+
+        dt = fps_clock.tick(60) / 1000
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = fps_clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
