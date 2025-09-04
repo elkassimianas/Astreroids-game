e060b6e5eb80ef6c243bc75ed40fba3c1eb9824a
ael-kass <aelkassimi321@gmail.com>
2025-09-05 00:10:37 +0100

    create the Asteroids and linear move

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps_clock = pygame.time.Clock()
+
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
 
@@ -18,11 +29,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill((0, 0, 0))
+
         dt = fps_clock.tick(60) / 1000
         updatable.update(dt)
-        for pl in drawable:
-            pl.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
