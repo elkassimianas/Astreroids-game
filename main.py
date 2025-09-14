aef5db4536e06e865188a1c9d2c7a19fb575ba69
ael-kass <aelkassimi321@gmail.com>
2025-09-14 13:12:58 +0100

    destroy Asteroid when the bullet hit it

--- main.py
+++ main.py
@@ -48,6 +48,11 @@ def main():
             if asteroid.is_colliding(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.is_colliding(shot):
+                    asteroid.kill()
+                    shot.kill()
+
 
 if __name__ == "__main__":
     main()
